368f3e9bda13314ed0429c0154e839705c44c9ab
BohdanMT <metelua@gmail.com>
2025-04-04 02:14:30 +0300

    Create User model extending AbstractUser with phone_number, email, and city fields

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -14,10 +15,21 @@ class City(models.Model):
     pass
 
 
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.phone_number}"
+
+    pass
+
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     watched = models.IntegerField(default=0)
