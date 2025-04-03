a068988d4c85ada99dfebbcac9e6c9e890929360
BohdanMT <metelua@gmail.com>
2025-04-04 02:38:00 +0300

    Update Comment model: add related_name for fields: author, article

--- organization/models.py
+++ organization/models.py
@@ -12,8 +12,6 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
-    pass
-
 
 class User(AbstractUser):
     phone_number = models.CharField(max_length=11, unique=True)
@@ -22,8 +20,6 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} {self.phone_number}"
-
-    pass
 
 
 class Article(models.Model):
@@ -37,16 +33,12 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-    pass
-
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comments")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.author.username
-
-    pass
