759714387f4ea50be97ead0d2a1f14f936481cba
BohdanMT <metelua@gmail.com>
2025-04-05 18:34:49 +0300

    Add ÐListView to User views and add empty template user_list

--- organization/views/user_views.py
+++ organization/views/user_views.py
@@ -4,10 +4,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from organization.models import User
 
+
+class UserListView(LoginRequiredMixin, generic.ListView):
+    model = User
+    template_name = "organization/user_list.html"
+    context_object_name = "users"
+    paginate_by = 10
+
+
 class UserDetailView(LoginRequiredMixin, generic.DeleteView):
     model = User
     template_name = "organization/user_detail.html"
     context_object_name = "user"
+
 
 class UserCreateView(LoginRequiredMixin, generic.CreateView):
     model = User
@@ -15,11 +24,13 @@ class UserCreateView(LoginRequiredMixin, generic.CreateView):
     fields = ["username", "email", "phone_number", "city", "password"]
     success_url = reverse_lazy("user-list")
 
+
 class UserUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = User
     template_name = "organization/user_form.html"
     fields = ["username", "email", "phone_number", "city"]
     success_url = reverse_lazy("user-list")
+
 
 class UserDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = User
