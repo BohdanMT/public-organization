7401d7c11f60e5352b80dea820c24577be060e81
BohdanMT <metelua@gmail.com>
2025-04-05 04:59:56 +0300

    Register models in admin panel

--- organization/admin.py
+++ organization/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import City, User, Article, Comment
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(User)
+admin.site.register(Article)
+admin.site.register(Comment)
